48c795a640f2957a8654951f76b73ae1d2a071ba
Pavel Averin <pasha2281337alfa@gmail.com>
2024-03-21 09:55:05 +0300

    test: add results for all the 4 functions

--- Task1/main.c
+++ Task1/main.c
@@ -11,7 +11,7 @@ double sin100x_cos100y(double x, double y) { return sin(100*x) * cos(100*y); }
 double d_sin100x_cos100y(double x, double y) { return -100 * sin(10 * x) * cos(10*y) -100 * sin(10*x)* cos(10* y); }
 
 double x2_y(double x, double y) {return pow(x,2) + y;}
-double d_x_y(double x, double y) {return 2;}
+double d_2x_y(double x, double y) {return 2;}
 
 double sqrt_of_x2_and_y2(double x, double y) {return sqrt(pow(x,2) + pow(y,2));}
 double d_sqrt_of_x2_and_y2(double x, double y) {return pow(pow(y,2)/(pow(x,2) + pow(y,2)),(3.0/2)) + pow(pow(x,2)/(pow(x,2) + pow(y,2)),(3.0/2));}
@@ -25,20 +25,24 @@ double d_exp100x2y2(double x, double y) { return  200 * (200 * pow(y,2) - 1) * p
 void experiment(function f, function u, int N) {
     net* net = initialize_net(N,64, f, u );
     clock_t begin = clock();
-    printf("net size %d, %d iterations\n",N, seidel(net, 0.01));
+    int result = seidel(net, 0.01);
     clock_t end = clock();
     float time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("%f\n", time_spent);
+    printf("%d\t%.6g\t%d\n",N, time_spent, result);
     delete_net(net);
 }
 
 int main() {
     srand (time ( NULL));
-    function u = exp100x2y2;
-    function f = d_exp100x2y2;
+    function u = e_x_e_y;
+    function f = d_e_x_e_y;
     omp_set_num_threads(8);
 
-    for (int i = 100; i <= 1000; i += 100) {
-        experiment(u,f,i);
+    for (int j = 1; j < 11; j++) {
+        printf("Launch %d\n", j);
+        for (int i = 100; i <= 1000; i += 100) {
+            experiment(u,f,i);
+        }
+
     }
 }
